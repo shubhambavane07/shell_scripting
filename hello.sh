06aeb6929431002a496e495340a95692cd8a7930
shubhambavane07 <testpic2525@gmail.com>
2024-09-12 17:22:37 +0530

    Commiting the changes of day01 i.e., hello.sh

---
+++ hello.sh
@@ -0,0 +1,9 @@
+#!/bin/bash 
+
+#The is script for demo 
+
+echo "First day of doing shell scripting from scratch"
+
+echo "These are the shell commands"
+
+echo "Writing the shell commands in file or in sequence in known as shell scripting"
